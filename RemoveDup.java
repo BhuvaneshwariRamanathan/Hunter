fb1a3aa13864a7f01ee07b4085320e4fcae54292
BhuvaneshwariRamanathan <ramansenthil3@gmail.com>
2016-12-09 16:12:17 +0530

    Update RemoveDup.java

--- RemoveDup.java
+++ RemoveDup.java
@@ -7,15 +7,22 @@ public class RemoveDup {
     Scanner s=new Scanner(System.in);
     String a=s.nextLine();
     String ans="";
+     boolean flag=true;
     for(int i=0;i<a.length();i++){
         if(a.indexOf(a.charAt(i))==a.lastIndexOf(a.charAt(i))){
+           flag=false;
             ans+=a.charAt(i)+"";
         }
         else{
             continue;
         }
     }
+     if(flag){
+     System.out.println(a);
+     }
+     else{
 System.out.println(ans);
+    }
     }
 
 }
