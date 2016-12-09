5bb64d87f252a016fa14c8dd8d56de1083b3f4b9
BhuvaneshwariRamanathan <ramansenthil3@gmail.com>
2016-12-09 18:34:13 +0530

    Update SecondLetter.java

--- SecondLetter.java
+++ SecondLetter.java
@@ -4,7 +4,13 @@ public static void main(String[] args) {
 Scanner s=new Scanner(System.in);
 System.out.println("Enter the String");
 String a=s.nextLine();
+    if(a.length()>=2){
 System.out.println(a.charAt(0)+""+Character.toUpperCase(a.charAt(1))+a.substring(2));
     }
+else{
+    System.out.println("There is no second letter");
+}
+}
+    
 
 }
