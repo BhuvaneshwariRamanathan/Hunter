b7c6742c05183b3cc4298beee1a80f7ac16bbd1b
BhuvaneshwariRamanathan <ramansenthil3@gmail.com>
2016-12-09 18:44:57 +0530

    Update SimpleInterest.java

--- SimpleInterest.java
+++ SimpleInterest.java
@@ -6,7 +6,12 @@ System.out.println("Enter the principle,number of year,rate of interest");
 float p=s.nextFloat();
 int n=s.nextInt();
 float r=s.nextFloat();
+  if(p>0 && n>0 && r>0){
 float simple=(p*n*r)/100f;
 System.out.println(simple);
+  }
+  else{
+    System.out.println("Positive values required");
+  }
 }
 }
