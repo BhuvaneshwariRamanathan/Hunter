922b5a262657af35f1f610375ac2340cee765017
BhuvaneshwariRamanathan <ramansenthil3@gmail.com>
2016-12-09 17:13:18 +0530

    Create AllPalindrome.java

---
+++ AllPalindrome.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class AllPalindrome{
+public static void main(String[] args) {
+Scanner s=new Scanner(System.in);
+System.out.println("Enter the string");
+String a=s.nextLine();
+    int count=0;
+    for(int i=0;i<a.length();i++){
+        for(int j=i+1;j<a.length();j++){
+            String str=a.substring(i,j+1);
+            StringBuffer rev=new StringBuffer(str);
+            if(str.equals(new String(rev.reverse()))){
+                System.out.println(str);
+                count++;
+            }
+        }
+    }
+    if(count==0){
+        System.out.println("No palindrome substrings are present");
+    }
+    }
+
+}
